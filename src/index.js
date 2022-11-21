36cb61b74edbf6fbc7ef8628089345b41e84937b
Cledson <cledson1996@gmail.com>
2022-11-21 12:18:04 -0300

    nuvem

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ app.use(registros);
 app.use(deletar);
 app.use(deletarsessao);
 
-app.listen(5000, () => {
-    console.log(`Server running in port: ${5000}`);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server running in port: ${port}`);
 });
